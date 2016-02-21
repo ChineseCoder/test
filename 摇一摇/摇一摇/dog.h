9169fc030c6a6e2825f33d7bc255f4d5881af886
LifeNoStop <maliangliang@maliangliangdemacbook-air.local>
2016-02-21 15:05:09 +0800

    添加dog类

---
+++ 摇一摇/摇一摇/dog.h
@@ -0,0 +1,13 @@
+//
+//  dog.h
+//  摇一摇
+//
+//  Created by maliangliang on 16/2/21.
+//  Copyright © 2016年 maliangliang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface dog : NSObject
+
+@end
---
+++ 摇一摇/摇一摇/dog.m
@@ -0,0 +1,13 @@
+//
+//  dog.m
+//  摇一摇
+//
+//  Created by maliangliang on 16/2/21.
+//  Copyright © 2016年 maliangliang. All rights reserved.
+//
+
+#import "dog.h"
+
+@implementation dog
+
+@end
